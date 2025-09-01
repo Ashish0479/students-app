4afa0f1080f9e7f5408d6ce1c2e1fa6cde91b7a6
Ashish <ashish@Ashishs-MacBook-Pro.local>
2025-09-01 14:25:54 +0530

    ...

--- src/validations/authValidator.js
+++ src/validations/authValidator.js
@@ -5,19 +5,19 @@ const { JWT_SECRET, COOKIE_SECURE} = require('../config/serverConfig');
 async function isLoggedIn(req, res, next) {
     
     const token = req.cookies["authToken"];
-    console.log(token);
+    console.log("token :",token);
     if(!token) {
         return res.status(401).json({
             success: false,
             data: {},
-            error: "Not authenticated",
+            error: "Not authenticated ,this error is from isLoggedin",
             message: "No Auth Token provided"
         });
     }
 
     try {
         const decoded = jwt.verify(token, JWT_SECRET);
-        console.log(token);
+        console.log("decoded token:",token);
         console.log(decoded, decoded.exp, Date.now() / 1000);
 
         if(!decoded) {
