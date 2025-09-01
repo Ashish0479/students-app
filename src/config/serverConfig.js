d8a533c9ff1070848a65d3c6f72bbe81993d0a0b
Ashish0479 <vp.ashish.2016@gmail.com>
2025-09-01 16:01:03 +0530

    Update serverConfig.js

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -8,5 +8,6 @@ module.exports={
     DB_URL:process.env.DB_URL,
     JWT_SECRET: process.env.JWT_SECRET,
     JWT_EXPIRY: process.env.JWT_EXPIRY,
-    FRONTEND_URL:process.env.FRONTEND_URL
+    FRONTEND_URL:process.env.FRONTEND_URL,
+    COOKIE_SECURE: process.env.NODE_ENV === 'production'
 }
